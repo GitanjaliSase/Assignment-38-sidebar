8d3e06e4b74d48815a26f1f5821743c4f7891b52
GitanjaliSase <sasegitanjali20@gmail.com>
2024-11-25 18:05:30 +0530

    css property add

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(){
+    const overlayElement=document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
+}
+function closeModel(){
+    const overlayElement=document.getElementById('overlay');
+    overlayElement.style.display = "none";
+}
