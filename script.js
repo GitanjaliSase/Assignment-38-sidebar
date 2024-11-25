84688d4c90b33b4c2079aef95a38079efca6dc0b
GitanjaliSase <sasegitanjali20@gmail.com>
2024-11-25 22:34:06 +0530

    sidebar added

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function closeModel(){
     const overlayElement=document.getElementById('overlay');
     overlayElement.style.display = "none";
 }
+
+function openSidebar() {
+    document.querySelector('.sidebar').style.left = '0';
+  }
+  
+  
+  function closeSidebar() {
+    document.querySelector('.sidebar').style.left = '-250px';
+  }
+  
